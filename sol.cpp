3978e8184dd438b9a0a4bbaa516591828ab2e8b6
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-07-20 13:30:19 +0800

    Add files via upload

---
+++ sol.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int limit = 1e5;
+vector<int> frequency(limit + 1);
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int n;
+		cin >> n;
+		int res = INT_MIN;
+		// find largest in the number (it will be the minimum number of 1s needed)
+		while (n > 0) {
+			res = max(res, n % 10);
+			n /= 10;
+		}
+		cout << res << '\n';
+	}
+	return 0;
+}
